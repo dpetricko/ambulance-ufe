b63df4c2f1b2c2ad53a9bf2db36ba61003915817
David Petricko <dpetricko@gmail.com>
2024-02-18 18:17:46 +0100

    E2E fixed

--- src/components/dp-ambulance-wl-list/test/dp-ambulance-wl-list.e2e.ts
+++ src/components/dp-ambulance-wl-list/test/dp-ambulance-wl-list.e2e.ts
@@ -1,17 +1,11 @@
-import { newSpecPage } from '@stencil/core/testing';
-import { DpAmbulanceWlList } from '../dp-ambulance-wl-list';
+import { newE2EPage } from '@stencil/core/testing';
 
-describe('dp-ambulance-wl-list', () => {
-   it('renders', async () => {
-      const page = await newSpecPage({
-         components: [DpAmbulanceWlList],
-         html: `<<pfx>-ambulance-wl-list></<pfx>-ambulance-wl-list>`,
-      });
+describe('abl-ambulance-wl-list', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+    await page.setContent('<dp-ambulance-wl-list></dp-ambulance-wl-list>');
 
-      const wlList = page.rootInstance as DpAmbulanceWlList;
-      const expectedPatients = wlList?.waitingPatients?.length;
-
-      const items = page.root.shadowRoot?.querySelectorAll("md-list-item");
-      expect(items?.length).toEqual(expectedPatients);
-   });
+    const element = await page.find('dp-ambulance-wl-list');
+    expect(element).toHaveClass('hydrated');
+  });
 });
